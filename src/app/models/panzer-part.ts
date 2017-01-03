f8d0b8d4cc4dda563b391873d4763ce699ed220c
VladimirSSS <volodymyr.suskyi@mev.com>
2017-01-03 18:43:13 +0200

    add services, not on

---
+++ src/app/models/panzer-part.ts
@@ -0,0 +1,9 @@
+export class PanzerPart {
+  constructor(
+    public id: string = null,
+    public name: string = null,
+    public src: string = null,
+    public number: number = null,
+    public price: number = null
+  ) { }
+}
---
+++ src/app/service/panzer-part.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { PanzerPart } from '../models/panzer-part';
+
+@Injectable()
+export class PanzerPartService {
+  private panzers: PanzerPart[] = [
+    {
+      'id': 'tiger_1',
+      'name': 'photo: Tiger-1',
+      'src': 'assets/images/tiger_1.jpg',
+      'number': 1,
+      'price': 300000
+    },
+    {
+      'id': 'tiger_2',
+      'name': 'photo: Tiger-2',
+      'src': 'assets/images/tiger_2.jpg',
+      'number': 2,
+      'price': 300000
+    },
+    {
+      'id': 'tiger_3',
+      'name': 'photo: Tiger-3',
+      'src': 'assets/images/tiger_3.jpg',
+      'number': 3,
+      'price': 300000
+    },
+    {
+      'id': 'tiger_4',
+      'name': 'photo: Tiger-4',
+      'src': 'assets/images/tiger_4.jpg',
+      'number': 4,
+      'price': 300000
+    },
+    {
+      'id': 'tiger_5',
+      'name': 'photo: Tiger-5',
+      'src': 'assets/images/tiger_5.jpg',
+      'number': 5,
+      'price': 300000
+    },
+    {
+      'id': 'tiger_6',
+      'name': 'photo: Tiger-6',
+      'src': 'assets/images/tiger_6.jpg',
+      'number': 6,
+      'price': 300000
+    }
+  ];
+
+  public getPanzerPart() {
+    return this.panzers;
+  }
+}
+
